fb99468de226eaeb4267a8d2c925f4943e23d2ca
xadet <xadetthree@gmail.com>
2014-09-15 12:53:17 +0100

    feature(dnsDomainIs): add built-in dnsDomainIs function

--- utils_test.go
+++ utils_test.go
@@ -10,3 +10,9 @@ func TestIsPlainHostName(t *testing.T) {
 	assert.True(t, isPlainHostName("www"), "'www' should be a valid plain host")
 	assert.False(t, isPlainHostName("www.netscape.com"), "'www.netscape.com' should not be a valid plain host")
 }
+
+func TestDnsDomainIs(t *testing.T) {
+	assert.True(t, dnsDomainIs("www.netscape.com", ".netscape.com"), "'www.netscape.com' should be a valid host for domain '.netscape.com'")
+	assert.False(t, dnsDomainIs("www", ".netscape.com"), "'www' should not be a valid host for domain '.netscape.com'")
+	assert.False(t, dnsDomainIs("www.mcom.com", ".netscape.com"), "'www.mcom.com' should not be a valid host for domain '.netscape.com'")
+}
